70f30df45f0d7deea535e045670a7dfb0b8cd910
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2026-01-03 14:25:29 +0300

    feat: add function to load single img with label

--- src/load.py
+++ src/load.py
@@ -3,13 +3,12 @@ import xml.etree.ElementTree as et
 
 IMG_NUM = 3686
 
-images = []
+def load_labeled_img(img_name: str) -> tuple[str, str]:
+    img = image.load_img(f'archive/images/{img_name}.png')
+    tree = et.parse(f'archive/annotations/{img_name}.xml')
+    root = tree.getroot()
+    label = root.find('.//object/name').text
+    return (img, label)
 
 for i in range(IMG_NUM):
-    img = image.load_img(f'archive/images/Cats_Test{i}.png')
-
-    tree = et.parse(f'archive/annotations/Cats_Test{i}.xml')
-    root = tree.getroot()
-    name = root.find('.//object/name').text
-
-    print(img, name)
+    print(load_labeled_img(f'Cats_Test{i}'))
