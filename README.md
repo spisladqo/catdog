3a5fb3633d0dfb156fbd708d106096e099f0a5f3
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2026-01-03 15:08:45 +0300

    feat: use imageio to load np array, label is int now

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Configure 3.12 python virtual environment and install the following libraries:
 pip install tensorflow==2.20
 pip install matplotlib==3.10.8
 pip install numpy==2.4
+pip install imageio==2.37.2
 ```
 after that, install and unarchive cat or dog dataset into the root of this directory:
 https://www.kaggle.com/datasets/andrewmvd/dog-and-cat-detection
--- src/load.py
+++ src/load.py
@@ -1,14 +1,27 @@
-from tensorflow.keras.preprocessing import image
 import xml.etree.ElementTree as et
+import imageio.v3 as iio
 
 IMG_NUM = 3686
+IMG_SIZE = 224
 
-def load_labeled_img(img_name: str) -> tuple[str, str]:
-    img = image.load_img(f'archive/images/{img_name}.png')
+imgs_with_labels = []
+
+
+def label_to_num(label:str) -> int:
+    if label == 'cat':
+        return 0
+    elif label == 'dog':
+        return 1
+    return -1
+
+
+def load_labeled_img(img_name: str) -> tuple[str, int]:
+    img = iio.imread(f'archive/images/{img_name}.png')
     tree = et.parse(f'archive/annotations/{img_name}.xml')
     root = tree.getroot()
-    label = root.find('.//object/name').text
+    label = label_to_num(root.find('.//object/name').text)
     return (img, label)
 
-for i in range(IMG_NUM):
-    print(load_labeled_img(f'Cats_Test{i}'))
+
+for i in range(10):
+    imgs_with_labels.append(load_labeled_img(f'Cats_Test{i}'))
